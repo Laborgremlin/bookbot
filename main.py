65ef68ee4e7cc8652e4395d65babedce4f084674
Labgremlin <fwunderle.biz@gmail.com>
2025-10-26 16:28:24 +0100

    added a function to stats.py that returns a sorted list of a dictionary; also added reporting utility to the main function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_char_count
+from stats import create_sorted_list
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -7,10 +8,19 @@ def get_book_text(path_to_book):
     return book_contents
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    path_to_book = "books/frankenstein.txt"
+    book_text = get_book_text(path_to_book)
     word_count = get_num_words(book_text)
     char_count = get_char_count(book_text)
+    sorted_char_count = create_sorted_list(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_book}...")
+    print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
-    print(char_count)
-    
+    print("--------- Character Count -------")
+    for item in sorted_char_count:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,7 @@
+def sort_on(items):
+    return items["num"]
+
+
 def get_num_words(book_text):
     words = book_text.split()
     word_count = len(words)
@@ -12,4 +16,11 @@ def get_char_count(book_text):
         else:
             char_count[char] = 1
     return char_count
+
+def create_sorted_list(char_count):
+    sorted_list = []
+    for key in char_count:
+        sorted_list.append({"char": key, "num": char_count[key]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
     
