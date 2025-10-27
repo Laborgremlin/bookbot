1208a3cc920ac7f4a7097e42741fce0bb891957e
Labgremlin <fwunderle.biz@gmail.com>
2025-10-27 19:00:10 +0100

    added the ability to specify a path to a book to be analyzed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_char_count
 from stats import create_sorted_list
+import sys
 
 def get_book_text(path_to_book):
     with open(path_to_book) as f:
@@ -8,7 +9,11 @@ def get_book_text(path_to_book):
     return book_contents
 
 def main():
-    path_to_book = "books/frankenstein.txt"
+    if len(sys.argv)!=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path_to_book = sys.argv[1]
     book_text = get_book_text(path_to_book)
     word_count = get_num_words(book_text)
     char_count = get_char_count(book_text)
